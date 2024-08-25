b46d300ce550b528173759a11d92d8a44c2aaf57
Lucas Jesus <luca.s@outlook.com>
2024-08-24 22:51:01 -0300

    criptografia e decriptografia prontas. Botões ajustados, projeto funcional.

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ function obterTexto() {
     return textoEntrada;
 }
 
+function criptografar(textoEntrada) {
+    textoSaida = textoEntrada
+    .replace(/e/g,"enter")
+    .replace(/i/g,"imes")
+    .replace(/a/g,"ai")
+    .replace(/o/g,"ober")
+    .replace(/u/g,"ufat");  
+    return textoSaida;
+    
+}
+
+function descriptografar(textoEntrada) {
+    textoSaida = textoEntrada
+    .replace(/enter/g,"e")
+    .replace(/imes/g,"i")
+    .replace(/ai/g,"a")
+    .replace(/ober/g,"o")
+    .replace(/ufat/g,"u");   
+    return textoSaida;
+    
+}
+
 function ocultarCampo(id) {
     document.getElementById(id).style.display = "none";
 }
@@ -14,14 +36,27 @@ function alterarEstiloClasse(id, propriedade, valor){
     /* estamos acessando a propriedade através de [elementos dinâmicos], cuja notação dispensa o uso de pontos. São úteis para quando não sabemos qual valor será usado na função */ 
 }
 
-function exibeResultado() {
+function exibeResultado(textoSaida) {
     ocultarCampo('principal__saida__img__empty');
     ocultarCampo('principal__saida__resultado__titulo');
     ocultarCampo('principal__saida__resultado__texto');
     /* entender posteriormente como evitar a chamada 3x */
     alterarEstiloClasse('p1','justify-content','flex-start');
-    let textoSaida = obterTexto();
+    /*let textoSaida = obterTexto();*/
     let campo = document.getElementById('principal__saida__resultado_valor');
     campo.innerHTML = textoSaida;
 }
 
+function iniciarCriptografia() {
+    let textoEntrada = obterTexto();
+    let textoSaida = criptografar(textoEntrada);
+    exibeResultado(textoSaida);
+
+    /* O erro encontrado foi que as funções estavam sendo chamadas sequencialmente, porém sem os devidos parâmetros de entrada corretos. Agora, elas foram ajustadas para serem ativadas apenas se tiverem parâmetros de entrada. Além disso estão sendo chamadas uma a uma, na sequência correta respeitando a ordem de disponibilidade das variáveis que são necessárias para sua devida execução */
+}
+
+function iniciarDesriptografia() {
+    let textoEntrada = obterTexto();
+    let textoSaida = descriptografar(textoEntrada);
+    exibeResultado(textoSaida);
+}
